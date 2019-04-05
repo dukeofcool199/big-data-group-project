4bda790c665a9e9037e1969f698dfedc0fd68f62
jenkin schibel <jenkin.schibel@protonmail.com>
2019-04-04 19:29:22 -0700

    got a basic graph to show up

--- games.r
+++ games.r
@@ -9,6 +9,5 @@ games = read.csv("games.csv", header=TRUE)
 summary(games)
 
 # excludes outliers that are far out there
-hist(games$Global_Sales, main="Global Sales", col="blue",xlim=c(0,8), ylab="Number of Games", xlab="Copies Sold (in millions)", breaks=100)
+plot(y=games$Global_Sales,x=games$Rating, main="Global Sales", col="blue",ylab="Number of Games", xlab="Copies Sold (in millions)", breaks=100)
 
-rmarkdown::draft("dashboard.r",template = "flex_dashboard", package = "flexdashboard")
